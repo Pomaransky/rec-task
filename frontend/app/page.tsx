34f860663528c7010fcbc67dab88c45c2e644bad
Arkadiusz Pomarański <pomaransky97@gmail.com>
2026-09-25 10:22:16 +0200

    Deplot on github pages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 import { ProductsView } from "@/components/features/products/products-view";
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col px-4 py-4">
       <div className="mx-auto w-full max-w-[1240px]">
-        <ProductsView />
+        <Suspense>
+          <ProductsView />
+        </Suspense>
       </div>
     </main>
   );
